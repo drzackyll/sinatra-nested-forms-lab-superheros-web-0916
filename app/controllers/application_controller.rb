d1786f75f20290989d2d2d7e3e3062f039fce220
drzackyll <adams.z.d@gmail.com>
2016-10-16 17:17:28 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,14 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :index
+    end
+
+    post '/' do
+      # binding.pry
+      @heroes = params
+      erb :team
+    end
 
 end
